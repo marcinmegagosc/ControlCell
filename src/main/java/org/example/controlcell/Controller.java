a355dc3bb81b8ce1c6491655260fe51a6d4277d7
marcinmegagosc <marcin.sss221@gmail.com>
2025-03-18 13:39:03 +0100

    dfgfgfgf

--- src/main/java/org/example/controlcell/Controller.java
+++ src/main/java/org/example/controlcell/Controller.java
@@ -1,14 +1,17 @@
 package org.example.controlcell;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.shape.Rectangle;
 
 import javax.imageio.IIOImage;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -31,6 +34,14 @@ public class Controller implements Initializable {
 
     @FXML
     private Label lblInfo;
+    @FXML
+    private Slider slider;
+    @FXML
+    private ScrollBar scrollBar;
+    @FXML
+    private Rectangle rect;
+    @FXML
+    private Label lblSize;
 
 
     Image dog = new Image(String.valueOf(getClass().getResource("/images/pies.jpg")));
@@ -51,6 +62,25 @@ public class Controller implements Initializable {
         radioTurtle.setOnAction(actionEvent -> {
             ImageView.setImage(turtle);
             lblInfo.setText("Zaznaczono zdjęcie z " + radioTurtle.getText());
+        });
+        slider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                rect.setWidth(300 *slider.getValue()/slider.getMax());
+                rect.setHeight(240 *slider.getValue()/slider.getMax());
+                scrollBar.setValue(slider.getValue());
+
+            }
+        });
+
+        scrollBar.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                rect.setWidth(300 *scrollBar.getValue()/scrollBar.getMax());
+                rect.setHeight(240 *scrollBar.getValue()/scrollBar.getMax());
+                slider.setValue(scrollBar.getValue());
+
+            }
         });
     }
 }
